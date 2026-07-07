c2bc60a877b504b2863356720a9ade45c43e6072
Armen96 <barsegyan96armen@gmail.com>
2026-07-07 14:32:56 +0400

    map

---
+++ lessons/02_variables_and_types/map.dart
@@ -0,0 +1,15 @@
+void main() {
+  var obj = {
+    'key': 'ca'
+  };
+  print(obj);
+
+  Map<String, String> planets = {
+    'first': 'mercury',
+    'second': 'venus'
+  };
+
+  print(planets);
+  print(planets['first']);
+  print(planets.isEmpty);
+}
